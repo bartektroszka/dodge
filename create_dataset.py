0b5d31a1b03baad64b245b00c03e9af634d2b7d1
btroszka <btroszka@alan-systems.local>
2021-02-11 15:55:20 +0100

    getting matches function implemented

--- create_dataset.py
+++ create_dataset.py
@@ -2,7 +2,6 @@ import requests, random
 
 APIKey = "RGAPI-83541200-22a7-4baf-9469-c90dc1667f41"
 region = "EUN1"
-summonerName = "Siwuuuuus"
 HEADERS = {
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -27,4 +26,16 @@ def request_players_ids(tier, division, max_num, chosen_num, queue = 'RANKED_SOL
         page_counter += 1
     return random.sample(ids, chosen_num)
 
-print(request_players_ids('PLATINUM', 'II', 10000, 3))
+def request_match_history(player_id, chosen_num):   
+    acc_URL = f'https://{region}.api.riotgames.com/lol/summoner/v4/summoners/{player_id}'
+    acc_response = requests.get(url=acc_URL, headers = HEADERS)
+    if acc_response is None:
+        return None
+    account_id response.json()['accountId']
+    #420 because it is ranked SOLO 5v5
+    URL = f'https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{account_id}?queue=420'
+    response = requests.get(url=acc_URL, headers = HEADERS)
+    match_list = response.json()['matches']
+    match_ids = [m['gameId'] for m in match_list]
+    return random.sample(ids, chosen_num)
+
